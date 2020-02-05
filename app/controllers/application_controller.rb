33720365349d1749bcb0a06e63b7383886afe11f
michaelg <sirgillem@gmail.com>
2020-02-05 21:32:08 +1100

    Create basic pages for initial deployment.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,14 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
+  # Temporary homepage
+  def home
+    render html: 'Home page under construction'
+  end
+
+  # Temporary other page
+  def temp
+    render html: 'Catalogue under construction'
+  end
 end
