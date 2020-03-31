70545922768808a1942119fd331d8091d6973768
michaelg <sirgillem@gmail.com>
2020-03-31 17:16:13 +1100

    Remove vestigial route for users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
-  get '/signup', to: 'users#new'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
