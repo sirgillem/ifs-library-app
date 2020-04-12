30b48ab01b4022f48b4c4f0063a5de3ecfb3802c
michaelg <sirgillem@gmail.com>
2020-04-12 22:10:33 +1000

    Fill out publisher index and new forms

--- test/models/publisher_test.rb
+++ test/models/publisher_test.rb
@@ -1,7 +1,13 @@
 require 'test_helper'
 
 class PublisherTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Publisher needs a name' do
+    new_pub = Publisher.new(name: '', website: 'www.example.com')
+    assert_not new_pub.valid?
+  end
+
+  test "Publisher doesn't need a website" do
+    new_pub = Publisher.new(name: 'Foo productions', website: '')
+    assert new_pub.valid?
+  end
 end
