fb3759f67ca9e68e7a7551a5b5bbf156c3288064
michaelg <sirgillem@gmail.com>
2020-04-12 22:26:08 +1000

    Add protocol prefix to URL if missing in database

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,4 +1,13 @@
 # Record information on publishers
 class Publisher < ActiveRecord::Base
   validates :name, presence: true
+
+  # Define a full URL for the website so it can be linked to correctly
+  def full_url
+    if website.match(/[A-Za-z]:\/\//)
+      website
+    else
+      "http://#{website}"
+    end
+  end
 end
--- test/models/publisher_test.rb
+++ test/models/publisher_test.rb
@@ -10,4 +10,15 @@ class PublisherTest < ActiveSupport::TestCase
     new_pub = Publisher.new(name: 'Foo productions', website: '')
     assert new_pub.valid?
   end
+
+  test 'Full URL is calculated if no protocol header' do
+    new_pub = Publisher.new(name: '1', website: 'www.example.com')
+    assert_equal 'http://www.example.com', new_pub.full_url
+  end
+
+  test 'Full URL handles protocol header in database' do
+    url = 'https://www.example.com'
+    new_pub = Publisher.new(name: '2', website: url)
+    assert_equal url, new_pub.full_url
+  end
 end
