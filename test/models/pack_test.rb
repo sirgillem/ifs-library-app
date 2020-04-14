3baf865bcaf4eda1e6889418a55779bad3da924f
michaelg <sirgillem@gmail.com>
2020-04-14 18:20:30 +1000

    Add Pack model tests

--- test/models/pack_test.rb
+++ test/models/pack_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class PackTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Pack needs a name' do
+    new_pack = Pack.new(name: '    ', publisher: Publisher.first, serial: '1')
+    assert_not new_pack.valid?
+  end
+
+  test "Pack doesn't need a serial number" do
+    new_pack = Pack.new(name: 'New', publisher: Publisher.first)
+    assert new_pack.valid?
+  end
+
+  test 'Pack needs a valid publisher' do
+    new_pack = Pack.new(name: 'New')
+    assert_not new_pack.valid?
+  end
+
+  test 'Valid pack can be created with all fields' do
+    new_pack = Pack.new(name: 'New', publisher: Publisher.first, serial: 'XY')
+    assert new_pack.valid?
+  end
 end
