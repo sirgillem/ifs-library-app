c9ce6bb88cd7e495e851c69c34a5039f5d845cb1
michaelg <sirgillem@gmail.com>
2020-04-14 19:34:03 +1000

    Fix publisher has_many packs relation

--- app/models/publisher.rb
+++ app/models/publisher.rb
@@ -1,6 +1,6 @@
 # Record information on publishers
 class Publisher < ActiveRecord::Base
-  has_many :pack
+  has_many :packs
   validates :name, presence: true
 
   # Define a full URL for the website so it can be linked to correctly
