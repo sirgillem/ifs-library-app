9293268e5108dfcd45296c124aec41f9a6fe11b8
michaelg <sirgillem@gmail.com>
2020-04-14 18:13:54 +1000

    Make pack columns NOT NULL where required

--- db/migrate/20200414080639_create_packs.rb
+++ db/migrate/20200414080639_create_packs.rb
@@ -1,8 +1,8 @@
 class CreatePacks < ActiveRecord::Migration
   def change
     create_table :packs do |t|
-      t.string :name
-      t.references :publisher, index: true, foreign_key: true
+      t.string :name, null: false
+      t.references :publisher, index: true, foreign_key: true, null: false
       t.string :serial
 
       t.timestamps null: false
