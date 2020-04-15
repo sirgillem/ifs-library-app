b79abbca8852f60ef127b63677dca08c5c22420d
michaelg <sirgillem@gmail.com>
2020-04-15 18:02:28 +1000

    Add authentication to songs controller

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,4 +1,6 @@
 class SongsController < ApplicationController
+  before_action :logged_in?
+  before_action :librarian?, only: [:new, :create, :edit, :update, :destroy]
   before_action :set_song, only: [:show, :edit, :update, :destroy]
 
   # GET /songs
@@ -70,5 +72,15 @@ class SongsController < ApplicationController
     # Never trust parameters from the scary internet, only allow the white list through.
     def song_params
       params.require(:song).permit(:title, :label, :publisher_id, :serial, :details, :notes_perf, :notes_lib, :pack_id, :recording, :style, :duration, :tempo, :purchased_at)
+    end
+
+    # Check that a user is logged in before showing anything
+    def logged_in?
+      redirect_to '/' unless current_user
+    end
+
+    # Check if the current user is a librarian before modifying anything
+    def librarian?
+      redirect_to songs_path unless current_user && current_user.librarian?
     end
 end
