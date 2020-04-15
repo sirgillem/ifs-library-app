17c0ec4bd32011d529312b124b4898a8e5087b28
michaelg <sirgillem@gmail.com>
2020-04-15 17:55:50 +1000

    Fix song fixture names to not use '-'. Add tests for song model validations.

--- test/models/song_test.rb
+++ test/models/song_test.rb
@@ -1,7 +1,41 @@
 require 'test_helper'
 
 class SongTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @song = songs(:opus_one)
+  end
+
+  test 'Song must have publisher or pack' do
+    assert @song.valid?
+    @song.publisher = nil
+    assert_not @song.valid?
+    @song.pack = packs(:easy_pack_1)
+    assert @song.valid?
+    @song.publisher = publishers(:pub1)
+    assert_not @song.valid?
+  end
+
+  test 'Song must have a title' do
+    assert @song.valid?
+    @song.title = '    '
+    assert_not @song.valid?
+  end
+
+  test 'Song tempo must be positive if it exists' do
+    @song.tempo = nil
+    assert @song.valid?
+    @song.tempo = -1
+    assert_not @song.valid?
+    @song.tempo = 1
+    assert @song.valid
+  end
+
+  test 'Song duration must be positive if it exists' do
+    @song.duration = nil
+    assert @song.valid?
+    @song.duration = -1
+    assert_not @song.valid?
+    @song.duration = 1
+    assert @song.valid
+  end
 end
