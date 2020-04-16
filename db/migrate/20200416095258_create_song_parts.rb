e67bbb0bd2c672d84208951d132604542058cf5c
michaelg <sirgillem@gmail.com>
2020-04-16 19:57:17 +1000

    Fix foreign key references for table prefix

--- db/migrate/20200416095258_create_song_parts.rb
+++ db/migrate/20200416095258_create_song_parts.rb
@@ -1,13 +1,15 @@
 class CreateSongParts < ActiveRecord::Migration
   def change
     create_table :song_parts do |t|
-      t.references :song, index: true, foreign_key: true
+      t.references :song, index: true
       t.string :name
       t.boolean :scanned
       t.text :notes
-      t.references :template, index: true, foreign_key: true
+      t.references :template, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :song_parts, :songs
+    add_foreign_key :song_parts, :song_templates
   end
 end
