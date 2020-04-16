d3380bb38288ed53a5812cd1deb1f8c819dcc805
michaelg <sirgillem@gmail.com>
2020-04-16 19:32:55 +1000

    Move new resources route for logical grouping

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :song_templates
   devise_for :users, skip: :registrations
   root 'static_pages#home'
   get '/contact', to: 'static_pages#contact'
   resources :publishers
   resources :packs
   resources :songs
+  resources :song_templates
   scope '/admin' do
     resources :users, only: [:index, :show, :edit, :update, :destroy]
   end
