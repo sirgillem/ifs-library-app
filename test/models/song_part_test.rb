eb9cd5abb010b742095664a58ce28ad574170f79
michaelg <sirgillem@gmail.com>
2020-04-16 20:15:34 +1000

    Song part model tests and fixtures

--- test/models/song_part_test.rb
+++ test/models/song_part_test.rb
@@ -1,7 +1,22 @@
 require 'test_helper'
 
 class SongPartTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'song part must have a name' do
+    p = song_parts(:opus_one_tenor1)
+    p.name = '    '
+    assert_not p.valid?
+    p.name = 'Tenor 1'
+    assert p.valid?
+  end
+
+  test 'song part must belong to a song xor template' do
+    p = song_parts(:opus_one_tenor1)
+    assert p.valid?
+    p.song = nil
+    assert_not p.valid?
+    p.template = templates(:bigband)
+    assert p.valid?
+    p.song = songs(:opus_one)
+    assert_not p.valid?
+  end
 end
