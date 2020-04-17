b05c127dd11c2b4c30d5ba40889ecc326e5f6896
michaelg <sirgillem@gmail.com>
2020-04-17 22:15:30 +1000

    Add ordering and full_text method to contributor relationship model. Add test and initial fixtures

--- app/models/contributor_relation.rb
+++ app/models/contributor_relation.rb
@@ -1,4 +1,10 @@
 class ContributorRelation < ActiveRecord::Base
+  default_scope -> { order(:sequence) }
   belongs_to :contributable, polymorphic: true
   belongs_to :person
+
+  # Get a descriptive string for the relationship
+  def full_text
+    "#{role} by #{person.full_name}"
+  end
 end
--- test/models/contributor_relation_test.rb
+++ test/models/contributor_relation_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class ContributorRelationTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'relations are returned in sequence' do
+    song = Song.create(title: 'Test Song')
+    c1 = ContributorRelation.create(contributable: song,
+                                    person: people(:johndoe),
+                                    role: 'Words and Music',
+                                    sequence: 10)
+    c2 = ContributorRelation.create(contributable: song,
+                                    person: people(:complexname),
+                                    role: 'Sponsored',
+                                    sequence: 5)
+    contribs = song.contributor_relations
+    assert_equal c2, contribs[0]
+    assert_equal c1, contribs[1]
+  end
+
+  test 'relation display name has role and person' do
+    contrib = contributor_relations(:one)
+    assert_match contrib.role, contrib.full_text
+    assert_match contrib.person.key_name, contrib.full_text
+  end
 end
