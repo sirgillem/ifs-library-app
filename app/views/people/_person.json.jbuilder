412430c3d4996274fea513b70126b73b1667a43f
michaelg <sirgillem@gmail.com>
2020-04-17 18:38:59 +1000

    Create scaffold for Person

---
+++ app/views/people/_person.json.jbuilder
@@ -0,0 +1,2 @@
+json.extract! person, :id, :pre_titles, :pre_names, :key_name_prefix, :key_name, :post_names, :key_name_suffix, :qualifications, :post_titles, :created_at, :updated_at
+json.url person_url(person, format: :json)
---
+++ db/migrate/20200417083822_create_people.rb
@@ -0,0 +1,16 @@
+class CreatePeople < ActiveRecord::Migration
+  def change
+    create_table :people do |t|
+      t.string :pre_titles
+      t.string :pre_names
+      t.string :key_name_prefix
+      t.string :key_name
+      t.string :post_names
+      t.string :key_name_suffix
+      t.string :qualifications
+      t.string :post_titles
+
+      t.timestamps null: false
+    end
+  end
+end
