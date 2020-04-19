e367e79526b5ec99d650f0295930a353204adb2e
michaelg <sirgillem@gmail.com>
2020-04-19 17:24:32 +1000

    Adjust nested form helper methods to allow specification of a separate location for the form partial.

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -13,31 +13,27 @@ module ApplicationHelper
   # This method creates a link with `data-id` `data-fields` attributes. These
   # attributes are used to create new instances of the nested fields through
   # Javascript.
-  def link_to_nested_fields(name, f, association, new_object, button_class)
+  def link_to_nested_fields(name, f, association, new_object, button_class,
+                            form_location = nil)
     # Saves the unique ID of the object into a variable.
     # This is needed to ensure the key of the associated array is unique. This
     # makes parsing the content in the `data-fields` attribute easier through
     # Javascript.
-    # We could use another method to achive this.
     id = new_object.object_id
 
-    # https://api.rubyonrails.org/ fields_for(record_name, record_object = nil,
-    # fields_options = {}, &block)
-    # record_name = :addresses
-    # record_object = new_object
-    # fields_options = { child_index: id }
-    # child_index` is used to ensure the key of the associated array is unique,
+    # `child_index` is used to ensure the key of the associated array is unique,
     # and that it matched the value in the `data-id` attribute.
-    # `person[addresses_attributes][child_index_value][_destroy]`
+    # `person[association_attributes][child_index_value][_destroy]`
     fields = f.fields_for(association, new_object, child_index: id) do |builder|
-      # `association.to_s.singularize + "_fields"` ends up evaluating to
-      # `address_fields`
-      # The render function will then look for
-      # `views/people/_address_fields.html.erb`
+      # The form may be located in some other view folder (e.g. shared). build
+      # this from the provided location - will be in
+      # `views/location/_association_fields.html.erb`. Default location is the
+      # controller for the overall form.
+      partial_parts = [form_location, association.to_s.singularize + '_fields']
+      form_partial = partial_parts.compact.join('/')
       # The render function also needs to be passed the value of 'builder',
-      # because `views/people/_address_fields.html.erb` needs this to render the
-      # form tags.
-      render(association.to_s.singularize + '_fields', f: builder)
+      # because the partial needs this to render the form tags.
+      render(form_partial, f: builder)
     end
 
     # This renders a simple link, but passes information into `data` attributes.
@@ -54,7 +50,7 @@ module ApplicationHelper
 
   # Create the link to add a new set of fields for the 'many' side of a
   # relation.
-  def link_to_add_fields(name, f, association)
+  def link_to_add_fields(name, f, association, form_location = nil)
     # Takes an object (@person) and creates a new instance of its associated
     # model (:addresses)
     # To better understand, run the following in your terminal:
@@ -63,14 +59,16 @@ module ApplicationHelper
     # new_object = @person.send(:addresses).klass.new
     new_object = f.object.send(association).klass.new
 
-    link_to_nested_fields(name, f, association, new_object, 'add_fields')
+    link_to_nested_fields(name, f, association, new_object, 'add_fields',
+                          form_location)
   end
 
   # Create the link to add a new set of fields for the 'one' side of a
   # relation
-  def link_to_new_fields(name, f, association)
+  def link_to_new_fields(name, f, association, form_location = nil)
     create_association = ['create', association].join('_')
     new_object = f.object.send(create_association.to_sym)
-    link_to_nested_fields(name, f, association, new_object, 'new_fields')
+    link_to_nested_fields(name, f, association, new_object, 'new_fields',
+                          form_location)
   end
 end
