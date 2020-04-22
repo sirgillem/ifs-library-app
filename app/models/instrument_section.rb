077503c159c3a63873e91c721248b8a36a887a5f
michaelg <sirgillem@gmail.com>
2020-04-22 18:14:49 +1000

    add model validations and methods to instrument section

--- app/models/instrument_section.rb
+++ app/models/instrument_section.rb
@@ -1,2 +1,8 @@
 class InstrumentSection < ActiveRecord::Base
+  validates :name, presence: true
+  validates :sequence, numericality: { greater_than_or_equal_to: 0 }
+
+  def to_s
+    name
+  end
 end
