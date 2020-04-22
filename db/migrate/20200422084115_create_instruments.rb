0aea3318564d3057e09262adb27273418c28f1b7
michaelg <sirgillem@gmail.com>
2020-04-22 18:43:40 +1000

    Fix foreign key assignment in migration, make instrument name not null

--- db/migrate/20200422084115_create_instruments.rb
+++ db/migrate/20200422084115_create_instruments.rb
@@ -1,10 +1,11 @@
 class CreateInstruments < ActiveRecord::Migration
   def change
     create_table :instruments do |t|
-      t.text :name
-      t.references :instrument_section, index: true, foreign_key: true
+      t.text :name, null: false
+      t.references :instrument_section, index: true
 
       t.timestamps null: false
     end
+    add_foreign_key :instruments, :instrument_sections
   end
 end
