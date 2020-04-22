30768dbceae207b8067d5e39be3c62b42d7fc75c
michaelg <sirgillem@gmail.com>
2020-04-22 18:22:13 +1000

    Add model tests for instrument sections

--- test/models/instrument_section_test.rb
+++ test/models/instrument_section_test.rb
@@ -1,7 +1,21 @@
 require 'test_helper'
 
 class InstrumentSectionTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'Section must have a name' do
+    s = InstrumentSection.new(name: '  ', sequence: 0)
+    assert_not s.valid?
+    s.name = 'Crowd'
+    assert s.valid?
+  end
+
+  test 'Section must have a sequence number' do
+    s = InstrumentSection.new(name: 'test', sequence: nil)
+    assert_not s.valid?
+    s.sequence = -1
+    assert_not s.valid?
+    s.sequence = 0
+    assert s.valid?
+    s.sequence = 1234
+    assert s.valid?
+  end
 end
