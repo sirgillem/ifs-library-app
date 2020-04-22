854000e157247dd397e98533b18d7293f91372c5
michaelg <sirgillem@gmail.com>
2020-04-22 19:21:12 +1000

    Initial instrument fixtures and model tests

--- test/models/instrument_test.rb
+++ test/models/instrument_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class InstrumentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'instrument must have a name' do
+    i = Instrument.new(name: '   ', section_id: instrument_sections(:sax).id)
+    assert_not i.valid?
+    i.name = 'foobar'
+    assert i.valid?
+  end
 end
