86808edb61aed72637c4b8d547fa0d62abe82cfe
michaelg <sirgillem@gmail.com>
2020-04-22 19:55:33 +1000

    Fix foreign key and nullable in pack instruments migration

--- db/migrate/20200422095153_create_part_instruments.rb
+++ db/migrate/20200422095153_create_part_instruments.rb
@@ -1,10 +1,12 @@
 class CreatePartInstruments < ActiveRecord::Migration
   def change
     create_table :part_instruments do |t|
-      t.references :part, index: true, foreign_key: true
-      t.references :instrument, index: true, foreign_key: true
+      t.references :part, index: true, null: false
+      t.references :instrument, index: true, null: false
 
       t.timestamps null: false
     end
+    add_foreign_key :part_instruments, :parts
+    add_foreign_key :part_instruments, :instruments
   end
 end
