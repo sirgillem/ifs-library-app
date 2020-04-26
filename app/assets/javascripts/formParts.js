73ca828a6a1b1443062bbfee23fb4de1d77bb75b
michaelg <sirgillem@gmail.com>
2020-04-26 15:16:21 +1000

    Fix up formatting in nested forms. Use select2 selection inputs in nested forms (requires fix to makethem initialise correctly).

--- app/assets/javascripts/formParts.js
+++ app/assets/javascripts/formParts.js
@@ -2,4 +2,15 @@
 $(document).ready(function() {
 	// Initialise select2 form inputs
 	$('.select-searchable').select2();
+
+	// Add event delegation to allow nested forms to get updated
+	document.addEventListener('click', e => {
+		$('.select-searchable').each(function() {
+			if ($(this).hasClass('select2-hidden-accessible')) {
+				// Select2 has been initialised
+			} else {
+				$(this).select2();
+			}
+		});
+	});
 });
