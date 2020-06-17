5f4e583c8c0a2520f1e6222b5fecd7f378753c05
michaelg <sirgillem@gmail.com>
2020-06-17 21:01:35 +1000

    Add sort indicator to header when sorted

--- app/assets/javascripts/table_tools.js
+++ app/assets/javascripts/table_tools.js
@@ -1,4 +1,11 @@
-// Sorting logic taken from https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+// Sorting logic adapted from https://stackoverflow.com/questions/14267781/sorting-html-table-with-javascript
+
+function stripSortIndicator(e) {
+	if (e.textContent.match('[\u25b2\u25bc]$')) {
+		e.textContent = e.textContent.slice(0,-2);
+	};
+};
+
 function makeTableSortable(){
 	const getCellValue = (tr, idx) => tr.children[idx].innerText || tr.children[idx].textContent;
 	
@@ -7,13 +14,21 @@ function makeTableSortable(){
 	    )(getCellValue(asc ? a : b, idx), getCellValue(asc ? b : a, idx));
 	
 	// do the work...
-	document.querySelectorAll('th.sortable').forEach(th => th.addEventListener('click', (() => {
-	    const table = th.closest('table').getElementsByTagName('tbody')[0];
-		console.log(table.querySelectorAll('tr').length)
-	    Array.from(table.querySelectorAll('tr'))
-	        .sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
-	        .forEach(tr => table.appendChild(tr) );
-	})));
+	document.querySelectorAll('th.sortable').forEach(th => {
+		th.addEventListener('click', (() => {
+	    		const table = th.closest('table').getElementsByTagName('tbody')[0];
+	    		Array.from(table.querySelectorAll('tr'))
+	        		.sort(comparer(Array.from(th.parentNode.children).indexOf(th), this.asc = !this.asc))
+	        		.forEach(tr => table.appendChild(tr) );
+			// We only sort by one column at a time, so show it that way
+			th.parentNode.querySelectorAll('th').forEach(th1 => stripSortIndicator(th1));
+			if (this.asc) {
+				th.textContent += " \u25b2";
+			} else {
+				th.textContent += " \u25bc";
+			};
+		}))
+	});
 };
 
 window.addEventListener('turbolinks:load', makeTableSortable);
